4d3d1af5dfe7a8f25ebc0c7d7140ab6c26302100
LemonJuiice <jaume.majov@gmail.com>
2024-04-29 17:36:36 +0200

    funcio jt

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ INTERFERANCE_PATTERN = []
 WALL_LIST = []    
 WAVE_LIST = []
 
+def fullJustify(words, maxWidth):
+    res, cur, num_of_letters = [], [], 0
+    for w in words:
+        if num_of_letters + len(w) + len(cur) > maxWidth:
+            for i in range(maxWidth - num_of_letters):
+                cur[i%(len(cur)-1 or 1)] += ' '
+            res.append(''.join(cur))
+            cur, num_of_letters = [], 0
+        cur += [w]
+        num_of_letters += len(w)
+    return res + [' '.join(cur).ljust(maxWidth)]
+
 def closeGame():
     pygame.quit()
     exit()
