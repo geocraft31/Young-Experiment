5c899aef2d8f21eee1b274c3fe43e912c2c947b1
LemonJuiice <jaume.majov@gmail.com>
2024-04-29 19:10:13 +0200

    Text "corregit"

--- main.py
+++ main.py
@@ -367,7 +367,7 @@ def main():
     textButton = Button(50, 300, "Explicació")
     escLabel = Label(47, 500, "Prem la tecla 'Esc' per tornar al menu principal", size=30, font='gloria')
     youngTitle = Label(550, 50, "EXPERIMENT\nDE\nYOUNG", True, 72)
-    autorsTitle = Label(550, 200, "Fet per: Jaume Majó i Josep Barnada", size=24, font="arial")
+    autorsTitle = Label(550, 200, "Fet per: Josep Barnada i Jaume Majó", size=24, font="arial")
     shrekAprovedImage = pygame.image.load("data/shrekApproved.jpg").convert()
     bonusButton = Button(50, 405, "Bonus")
     video33 = cv2.VideoCapture("data/FA33.mp4")
@@ -379,10 +379,10 @@ def main():
     pygame.mixer.init()
     
     # THANKS MENU
-    thanksText = Label(5, 5, multiline=True, size=28, font='Arial', text="""Hola Ivan, abans que surtis d'aquí et voldriem agraïr el teu esforç i la paciència. Esperem que t'ho hagis passat bé i hagis aprés molt al llarg d'aquesta experiència. Per últim, recorda que, tot i que l'animació pugui semblar senzilla, realment ha costat molt.
+    thanksText = Label(5, 5, multiline=True, size=28, font='Arial', text="""Hola Ivan, abans que surtis d'aquí et voldriem agrair el teu esforç i la paciència. Esperem que t'ho hagis passat bé i hagis aprés molt al llarg d'aquesta experiència. Per últim, recorda que, tot i que l'animació pugui semblar senzilla, realment ha costat molt.
 
 Agraïments:
-A l'Ivan, per haver confiat sempre en nosaltres i haver executat aquest programa (espero que l'hagis obert). També voldríem agrair a en Genís pel seu mitiquíssim "BRAWL STARS" durant l'explicació. A en Fernando Alonso, pel seu meravellós mewing. A en Mike Wazowski (Ivan 2) i l'Avestrús per ser els pilars fonamentals en la creació d'aquest treball. I, com no podia faltar, l'Shrek, pel seu segell de qualitat indispensable per a la certificació del treball. Evidentment, a en Thomas Young, que sense els seus experiments no hagués estat possible aquest treball. També volem agrair a en Miquel Àngel per no posar pràctiques d'història sobre el Tema 9: La restauració democràtica, ja que el temps que haguéssim utilitzat en fer les pràctiques (33 hores), l'hem pogut invertir en acabar el projecte de YOUNG a temps. Finalment, NO volem agrair a l'Anna Puigdevall per haver vingut a fer de policia a una classe de física i per dubtar de les capacitats del professor del mestre Jedi Ivan Fernandez III.
+A l'Ivan, per haver confiat sempre en nosaltres i haver executat aquest programa (espero que l'hagis obert). També voldríem agrair a en Genís pel seu mitiquíssim "BRAWL STARS" durant l'explicació. A en Fernando Alonso, pel seu meravellós mewing. A en Mike Wazowski (Ivan 2) i l'Avestrús (Ivan 3) per ser els pilars fonamentals en la creació d'aquest treball. I, com no podia faltar, l'Shrek, pel seu segell de qualitat indispensable per a la certificació del treball. Evidentment, a en Thomas Young, que sense els seus experiments no hagués estat possible aquest treball. També volem agrair a en Miquel Àngel per no posar pràctiques d'història sobre el Tema 9: La restauració democràtica, ja que el temps que haguéssim utilitzat en fer les pràctiques (33 hores), l'hem pogut invertir en acabar el projecte de YOUNG a temps. Finalment, NO volem agrair a l'Anna Puigdevall per haver vingut a fer de policia a una classe de física i per dubtar de les capacitats del professor del mestre Jedi Ivan Fernandez III.
 
 Ara sí que has arribat al final del treball, abans, però, com que sabem que faràs un cop surtis d'aquí, mira aquest últim vídeo.
                       """)
